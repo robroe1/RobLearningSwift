746288567ed8ff9a513675b97fde13d536e8c852
robroe1 <rroe@MadlyRad.com>
2021-02-16 00:43:39 -0500

    Pronouns button added

--- RobPlayground/Registration/PronounsRegistraton.swift
+++ RobPlayground/Registration/PronounsRegistraton.swift
@@ -11,6 +11,15 @@ class PronounsRegistraton: UIViewController {
 
     @IBOutlet var NextButton: UIButton!
     
+    
+    @IBOutlet var SheHerButton: UIButton!
+    
+    
+    @IBOutlet var HeHimButton: UIButton!
+    
+    
+    @IBOutlet var TheyThemButton: UIButton!
+    
     @IBOutlet var BackButton: UIButton!
     
     override func viewDidLoad() {
@@ -25,11 +34,31 @@ class PronounsRegistraton: UIViewController {
         
 
         
-        NextButton.layer.borderWidth = 2
+        SheHerButton.layer.borderWidth = 2
         
-        NextButton.layer.borderColor = UIColor.white.cgColor
+        SheHerButton.layer.borderColor = UIColor.white.cgColor
 
-        NextButton.layer.cornerRadius = NextButton.frame.height / 2
+        SheHerButton.layer.cornerRadius = NextButton.frame.height / 2
+        
+      
+        HeHimButton.layer.borderWidth = 2
+        
+        HeHimButton.layer.borderColor = UIColor.white.cgColor
+
+        HeHimButton.layer.cornerRadius = NextButton.frame.height / 2
+        
+
+        
+        TheyThemButton.layer.borderWidth = 2
+        
+        TheyThemButton.layer.borderColor = UIColor.white.cgColor
+
+        TheyThemButton.layer.cornerRadius = NextButton.frame.height / 2
+        
+        
+        
+        
+        
         
         // Do any additional setup after loading the view.
     }
