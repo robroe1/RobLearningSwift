e7b586457f588e4b22a282e80760c4f0fd6bc01c
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-02-21 17:39:22 -0800

    moved robs code to dif page

--- RobPlayground/ViewController.swift
+++ RobPlayground/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        print("Hey Rob it is here")
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        print("hey rob look before this")
+    
+    }
 }
+     
 
